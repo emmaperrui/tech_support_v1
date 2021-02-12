d24b1b95f897ed82178899687ac577e836f62094
Emma Perez <dinaisaur@gmail.com>
2021-02-12 09:30:02 +0100

    03 Generación de respuestas aleatorias en TechSupport

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -6,12 +9,25 @@
  * @version    0.1 (2011.07.31)
  */
 public class Responder
-{
+{   
+    //atributo de tipo random
+    private Random rnd = new Random();
+    //atributo de tripo String
+    private ArrayList<String> respuesta;
+
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        respuesta = new ArrayList<String>();
+        respuesta.add("Bienvenido al servicio técnico de DodgySoft");
+        respuesta.add("Cuéntenos su problema");
+        respuesta.add("Lo siento, no podemos ayudarle");
+        respuesta.add("Suena interesante, cuéntenos más");
+        respuesta.add("Por favor, repita de nuevo su pregunta");
+        respuesta.add("Espere por favor");
+        rnd = new Random();
     }
 
     /**
@@ -20,6 +36,6 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        return respuesta.get(rnd.nextInt(respuesta.size()));
     }
 }
