d87079f2d0e4c7886bfa2b99519c0dffa2f8a2bf
Emma Perez <dinaisaur@gmail.com>
2021-03-03 08:57:38 +0100

    06 respondiendo a conjuntos de respuestas con un HashSet

--- Responder.java
+++ Responder.java
@@ -2,7 +2,6 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 
 /**
  * The responder class represents a response generator object.
@@ -19,6 +18,16 @@ public class Responder
     private ArrayList<String> respuesta;
     //creo un hashmap
     HashMap<String,String> clave;
+    //hashmap secundario
+    HashMap<HashSet<String>,String> respuestaAuxiliar;
+    //respuesta 1
+    HashSet<String> respuesta1;
+    //respuesta 2
+    HashSet<String> respuesta2;
+    //respuesta 3
+    HashSet<String> respuesta3;
+    //respuesta 4
+    HashSet<String> respuesta4;
 
     /**
      * Construct a Responder - nothing to do
@@ -28,6 +37,11 @@ public class Responder
         rnd = new Random();
         respuesta = new ArrayList<String>();
         clave = new HashMap<String,String>();
+        respuesta1 = new HashSet<String>();
+        respuesta2 = new HashSet<String>();
+        respuesta3 = new HashSet<String>();
+        respuesta4 = new HashSet<String>();
+        respuestaAuxiliar = new HashMap<HashSet<String>, String>();
         respuesta.add("Bienvenido al servicio técnico de DodgySoft");
         respuesta.add("Cuéntenos su problema");
         respuesta.add("Lo siento, no podemos ayudarle");
@@ -38,6 +52,19 @@ public class Responder
         clave.put("horario", "nuestro horario lo podrá encontrar el la web");
         clave.put("teléfono", "666666666");
         clave.put("correo", "dodgysoft@gmail.com");
+        respuesta1.add("necesito");
+        respuesta1.add("más");
+        respuesta1.add("información");
+        respuesta2.add("adiós");
+        respuesta3.add("hola");
+        respuesta4.add("gracias");
+        respuesta4.add("por");
+        respuesta4.add("la");
+        respuesta4.add("información");
+        respuestaAuxiliar.put(respuesta1, "Para más información busque nuestro catálogo de ofertas");
+        respuestaAuxiliar.put(respuesta2, "Buenas tardes");
+        respuestaAuxiliar.put(respuesta3, "Buenos días");
+        respuestaAuxiliar.put(respuesta4, "¡Qué tenga un buen día!");
     }
 
     /**
@@ -47,13 +74,11 @@ public class Responder
     public String generateResponse(HashSet<String> userInput)
     {
         String respuestas = null;
-        for(String input : userInput){
-            if(clave.containsKey(input)){
-                respuestas = clave.get(input);
-            }
-            if(respuestas == null){
-                respuestas = respuesta.get(rnd.nextInt(respuesta.size()));
-            }
+        if(respuestaAuxiliar.containsKey(userInput)){
+            respuestas = respuestaAuxiliar.get(userInput);
+        }
+        if(respuestas == null){
+            respuestas = respuesta.get(rnd.nextInt(respuesta.size()));
         }
         return respuestas;
     }
